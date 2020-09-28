511ed3ff0157559d1291faf7d0f535564830e3e1
willtring96 <50152664+willtring96@users.noreply.github.com>
2020-09-28 11:27:17 -0700

    Update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -234,22 +234,22 @@ jQuery(function ($) {
     // -------------------------------------------------------------
     // Vidio auto play
     // -------------------------------------------------------------
-    (function () {
+//    (function () {
     
     /* Vimeo API: http://developer.vimeo.com/player/js-api */
-    
-        var iframe = document.getElementById('nofocusvideo');
-        // $f == Froogaloop
-        var player = $f(iframe);
-
-        $('.modal').on('hidden.bs.modal', function () {
-        player.api('pause');
-        })
-
-        $('.modal').on('shown.bs.modal', function () {
-        player.api('play');
-        })
-    }());
+//    
+//        var iframe = document.getElementById('nofocusvideo');
+//        // $f == Froogaloop
+//        var player = $f(iframe);
+//
+//        $('.modal').on('hidden.bs.modal', function () {
+//        player.api('pause');
+//        })
+//
+//        $('.modal').on('shown.bs.modal', function () {
+//        player.api('play');
+//        })
+//    }());
 
 
 
@@ -393,8 +393,3 @@ jQuery(function ($) {
 
 
 });
-
-
-
-
-
